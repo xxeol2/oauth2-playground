563cc02306d2c3c0c433dafc5c1370c08b0fd3a7
xxeol2 <jhj01177@naver.com>
2023-08-03 20:44:14 +0900

    init: project init

---
+++ src/test/java/ash/oauth/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package ash.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
